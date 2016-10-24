ed66d8fae2a01f1e67e276fdee1e204af1209a46
Keomony Khun <keomony@gmail.com>
2016-10-24 13:32:15 +0100

    adding more information about students

--- directory.rb
+++ directory.rb
@@ -1,16 +1,16 @@
 #let's put all students into an array
 students = [
-"Dr. Hannibal Lecter",
-"Darth Vader",
-"Nurse Ratched",
-"Michael Corleone",
-"Alex Delarge",
-"The Wicked Witch of the West",
-"Terminator",
-"Freddy Krueger",
-"The Joker",
-"Joffrey Baratheon",
-"Norman Bates"
+["Dr. Hannibal Lecter", :november],
+["Darth Vader", :november],
+["Nurse Ratched", :november],
+["Michael Corleone", :november],
+["Alex Delarge", :november],
+["The Wicked Witch of the West", :november],
+["Terminator", :november],
+["Freddy Krueger", :november],
+["The Joker", :november],
+["Joffrey Baratheon", :november],
+["Norman Bates", :november]
 ]
 
 def print_header
@@ -18,9 +18,9 @@ def print_header
   puts "-".* 20
 end
 
-def print(names)
-  names.each do |name|
-    puts name
+def print(students)
+  students.each do |student|
+    puts "#{student[0]} (#{student[1]} cohort)"
   end
 end
 
