6ebd5dde91ea702b5376dfb0794c8f071e1249f5
Keomony Khun <keomony@gmail.com>
2016-10-28 14:52:27 +0100

    10. refactoring the code

--- directory.rb
+++ directory.rb
@@ -1,50 +1,54 @@
-#set months as global variable as it is needed in different methods
+@students = [] # an empty array accessible to all methods
+#set months as an instance variable as it is needed in different methods within the class
 #months of the year are always the same, there's no modification on the varialbe and its values
-$months = ['January','Feburary','March','April','May','June','July','August','September','November','December']
+@months = ['January','Feburary','March','April','May','June','July','August','September','November','December']
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. List of all students whose names begin with \'k\'"
+  puts "4. Print students whose names are shorter than 12 characters"
+  puts "5. Print students grouped by cohort"
+  puts "9. Exit" # 9 because we'll be adding more items
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "3"
+      puts "-".* 70
+      print_name_begins_with('k')
+    when "4"
+      puts "-".* 70
+      print_name_shorter_than(12)
+    when "5"
+      puts "-".* 70
+      print_groupby_cohort
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant. Try again."
+  end
+end
 
 def interactive_menu
-  #create an empty array
-  students = []
-  # 4. repeat from step 1
   loop do
-    #1. print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "3. List of all students whose names begin with \'k\'"
-    puts "4. Print students whose names are shorter than 12 characters"
-    puts "5. Print students grouped by cohort"
-    puts "9. Exit" # 9 because we'll be adding more items
-    # 2. read the input and save it into a variable
-    selection = gets.chomp
-    # 3. do what the user has asked
-    case selection
-      when "1"
-        students = input_students
-      when "2"
-        print_header
-        print(students)
-        print_footer(students)
-      when "3"
-        puts "-".* 70
-        print_name_begins_with(students, 'k')
-      when "4"
-        puts "-".* 70
-        print_name_shorter_than(students, 12)
-      when "5"
-        puts "-".* 70
-        print_groupby_cohort(students)
-      when "9"
-        exit
-      else
-        puts "I don't know what you meant. Try again."
-    end
+    print_menu
+    process(gets.chomp)
   end
 end
 
 #input students info from the user and do some validations
 def input_students
-  #create an empty array
-  students = []
   puts "To finish, just hit return twice"
   puts "Please enter the names of the students"
   puts "Name:"
@@ -58,7 +62,7 @@ def input_students
     cohort = gets.gsub(/\s+$/,'')
     #validate cohort input if it's empty or not match with the existing cohort (considered it as typo)
     #, print out Typo to let the user know
-    if (cohort.to_s.empty?) || ($months.select{|m| m.downcase == cohort.downcase}.empty?) == true
+    if (cohort.to_s.empty?) || (@months.select{|m| m.downcase == cohort.downcase}.empty?) == true
       puts "Typo"
     else
       puts "Hobbies:"
@@ -69,19 +73,17 @@ def input_students
       puts "Height:"
       height = gets.gsub(/\s+$/,'')
       #add the student hash to the array
-      students << {name: name, cohort: cohort.capitalize.to_sym, hobbies: hobbies, country: country, height: height}
-      if students.length == 1
-        puts "Now we have #{students.count} student"
+      @students << {name: name, cohort: cohort.capitalize.to_sym, hobbies: hobbies, country: country, height: height}
+      if @students.length == 1
+        puts "Now we have #{@students.count} student"
       else
-        puts "Now we have #{students.count} students"
+        puts "Now we have #{@students.count} students"
       end
       #get another name from the user
       puts "Name:"
       name = gets.gsub(/\s+$/,'')
     end
   end
-  #return the array of students
-  students
 end
 
 #it returns true if name has some characters rather than spaces
@@ -94,17 +96,17 @@ def print_header
   puts "-".* 70
 end
 
-def print(students)
-  students.each_with_index do |student,i|
+def print_students_list
+  @students.each_with_index do |student,i|
     puts "#{i+1}.#{student[:name]} (#{student[:cohort]} cohort) (#{student[:hobbies]} hobbies) (#{student[:country]} country) (#{student[:height]} height)".center(50)
   end
 end
 
 #print the students whose name begins with a specific letter
-def print_name_begins_with(students, a_letter)
+def print_name_begins_with(a_letter)
   puts "Students whose names begin with #{a_letter}".center(50)
   #if array is empty, print this sentence and return
-  if students.length == 0
+  if @students.length == 0
     puts "There's no student in the array.".center(50)
     return
   end
@@ -113,12 +115,12 @@ def print_name_begins_with(students, a_letter)
   #iterator starts from 1
   i = 1
   begin
-    if students[j][:name].chars.first.downcase == a_letter.downcase
-      puts "#{i}.#{students[j][:name]} (#{students[j][:cohort]} cohort)".center(50)
+    if @students[j][:name].chars.first.downcase == a_letter.downcase
+      puts "#{i}.#{@students[j][:name]} (#{@students[j][:cohort]} cohort)".center(50)
       i +=1
     end
     j += 1
-  end while j< students.length #while loops as long as the condition is true
+  end while j< @students.length #while loops as long as the condition is true
   #if no name matched the condition
   if i < 2
     puts "There's no student whose name beings with #{a_letter}".center(50)
@@ -126,10 +128,10 @@ def print_name_begins_with(students, a_letter)
 end
 
 #print students whose names are shorter than 12 characters
-def print_name_shorter_than(students, num)
+def print_name_shorter_than(num)
   puts "Students whose names are shorter than #{num} characters".center(50)
   #if array is empty, print this sentence and return
-  if students.length == 0
+  if @students.length == 0
     puts "There's no student in the array.".center(50)
     return
   end
@@ -138,23 +140,23 @@ def print_name_shorter_than(students, num)
   #iterator starts from 1
   i = 1
   begin
-    if students[j][:name].length < num
-      puts "#{i}.#{students[j][:name]}, #{students[j][:name].length} (#{students[j][:cohort]} cohort)".center(50)
+    if @students[j][:name].length < num
+      puts "#{i}.#{@students[j][:name]}, #{@students[j][:name].length} (#{@students[j][:cohort]} cohort)".center(50)
       i += 1
     end
     j +=1
-  end until j > students.length - 1 #until loops as long as the condition is false
+  end until j > @students.length - 1 #until loops as long as the condition is false
   #if no name matched the condition
   if i < 2
     puts "There's no student whose name is shorter than #{num}".center(50)
   end
 end
 
-def print_groupby_cohort(students)
-  cohorts = students.map{|student| student[:cohort]}.uniq
+def print_groupby_cohort
+  cohorts = @students.map{|student| student[:cohort]}.uniq
   cohorts.each do |cohort|
     puts "Cohort: #{cohort}".center(50)
-    students.each do |student|
+    @students.each do |student|
       if student[:cohort] == cohort
         puts "#{student[:name]}".center(50)
       end
@@ -162,8 +164,8 @@ def print_groupby_cohort(students)
   end
 end
 
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students.".center(50)
+def print_footer
+  puts "Overall, we have #{@students.count} great students.".center(50)
 end
 
 #nothing happens if we don't call the fuction
